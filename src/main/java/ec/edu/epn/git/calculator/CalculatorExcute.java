1683194b5c843e1c66269f2704caa316e0dbe8e6
FredBailon <naelbailonhot@outlook.com>
2024-10-28 00:23:03 -0500

    update

--- src/main/java/ec/edu/epn/git/calculator/CalculatorExcute.java
+++ src/main/java/ec/edu/epn/git/calculator/CalculatorExcute.java
@@ -1,4 +1,11 @@
 package ec.edu.epn.git.calculator;
 
 public class CalculatorExcute {
+    public static void main(String[] args){
+        System.out.println("-- Calculator Excute --");
+
+        Calculator c = new Calculator();
+        int addition = c.sumar(4,7);
+        System.out.println("c.sumar(4,7) = " + addition);
+    }
 }
